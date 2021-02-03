630ff261a74bfdee0e484aee3ceff95b5164dc68
Abir <abir@cse.iitkgp.ac.in>
2021-02-03 11:24:23 +0530

    Pythonic way of encapsulation shown here

--- 03_Class/Door_Standard.py
+++ 03_Class/Door_Standard.py
@@ -5,3 +5,23 @@ class Door:
     
     def get_number(self):
         return self._number
+    
+    def set_number(self, number):
+        self._number = number
+    
+    def get_status(self):
+        return self._status
+    
+    def set_status(self, status):
+        self._status = status
+    
+    def open(self):
+        self.status = 'open'
+    
+    def close(self):
+        self.status = 'closed'
+    
+    
+        
+        
+        
