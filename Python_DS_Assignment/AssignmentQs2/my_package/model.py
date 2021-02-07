6266d36929cc21734eb477244fd16fcd00336bd4
Siddhant Agarwal <agarwalsiddhant10@gmail.com>
2021-02-07 18:16:59 +0530

    Bug fixes

--- Python_DS_Assignment/AssignmentQs2/my_package/model.py
+++ Python_DS_Assignment/AssignmentQs2/my_package/model.py
@@ -54,9 +54,6 @@ class ObjectDetectionModel(object):
 		pred_class = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(predictions[0]['labels'].numpy())] # Get the Prediction Score
 		pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(predictions[0]['boxes'].detach().numpy())] # Bounding boxes
 		pred_score = list(predictions[0]['scores'].detach().numpy())
-		pred_t = [pred_score.index(x) for x in pred_score if x > threshold][-1] # Get list of index with score greater than threshold.
-		pred_boxes = pred_boxes[:pred_t+1]
-		pred_class = pred_class[:pred_t+1]
 		
 		return pred_boxes, pred_class, pred_score
 
